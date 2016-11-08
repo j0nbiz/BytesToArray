e5f8de0ff6cab80b02ae69cb4859198efa0c8fa8
Erika Bourque <erika.bourque@dawsoncollege.qc.ca>
2016-11-08 10:07:01 -0500

    Initial commit

---
+++ ArrayToDouble/src/arraytodouble/DoubleToBytes.java
@@ -0,0 +1,54 @@
+
+/**
+ *  Converting a double to an eight-byte array and back to a double.
+ *
+ *  @author     Alex Simonelis
+ *  @version    October, 2011
+ *
+ */
+
+
+public class DoubleToBytes
+{
+    public static void main(String s[])
+    {
+		double d =123.8;
+		System.out.println("**** "+ d);
+		d = byteArrayToDouble(doubleToByteArray(d));
+		System.out.println("**** " + d);
+	}
+
+
+	public static byte [] doubleToByteArray (double numDouble)
+	{
+		byte [] arrayByte = new byte [8];
+		long numLong;
+
+		numLong = Double.doubleToRawLongBits(numDouble);
+
+
+
+
+
+
+
+
+
+		return arrayByte;
+	}
+
+	public static double byteArrayToDouble (byte [] arrayByte)
+	{
+		double numDouble;
+		long numLong;
+
+		numLong = (((long)arrayByte[0] & 0x00000000000000FFL) << 56) | (((long)arrayByte[1] & 0x00000000000000FFL) << 48) |
+			(((long)arrayByte[2] & 0x00000000000000FFL) << 40) |	(((long)arrayByte[3] & 0x00000000000000FFL) << 32) |
+			(((long)arrayByte[4] & 0x00000000000000FFL) << 24) | (((long)arrayByte[5] & 0x00000000000000FFL) << 16) |
+			(((long)arrayByte[6] & 0x00000000000000FFL) <<  8) | ((long)arrayByte[7] & 0x00000000000000FFL);
+
+		numDouble = Double.longBitsToDouble(numLong);
+
+		return numDouble;
+	}
+}
